55836989a3a2597e19ee3bd592485514c742a72e
ndudar <ndudar@gmail.com>
2023-01-22 16:44:46 -0600

    feat: popovers now change variant on dipstick

--- src/features/DipStick/DipStick.js
+++ src/features/DipStick/DipStick.js
@@ -1,3 +1,4 @@
+import React, { useState, useRef } from "react";
 import Button from "react-bootstrap/Button";
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
@@ -16,35 +17,45 @@ const DipStick = () => {
     flexBasis: "100%"
   }
 
+  const target = useRef(null)
+
+  const handleClick = (val, newVariant) => {
+    target.current.innerHTML = "X";
+    target.current.className = `rounded-circle btn btn-${newVariant}`
+  }
+
   const popover = (
-    <Popover>
+    <Popover target={target.current}>
       <Popover.Body>
         <ButtonGroup>
-        <Button variant="success">Yes</Button>
-        <Button variant="warning">Kinda</Button>
-        <Button variant="danger">No</Button>
+        <Button onClick={() => {handleClick(100, 'success')}} value={100} variant="success">Yes</Button>
+        <Button onClick={() => {handleClick(50, 'warning')}} value={50} variant="warning">Kinda</Button>
+        <Button onClick={() => {handleClick(0, 'danger')}} value={0} variant="danger">No</Button>
         </ButtonGroup>
       </Popover.Body>
     </Popover>
   )
 
+  //target.current.innerHTML === '!'
+  //target.current.className ==="rounded-circle btn btn-outline-dark"
+
   return (
     <>
     <div style={dipStickStyle}>
       <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-      <Button className="rounded-circle" variant="outline-dark">!</Button>
+      <Button ref={target} className="rounded-circle" variant='outline-dark'>!</Button>
       </OverlayTrigger>
       <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-      <Button className="rounded-circle" variant="outline-dark">!</Button>
+      <Button ref={target} className="rounded-circle" variant='outline-dark'>!</Button>
       </OverlayTrigger>
       <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-      <Button className="rounded-circle" variant="outline-dark">!</Button>
+      <Button ref={target} className="rounded-circle" variant='outline-dark'>!</Button>
       </OverlayTrigger>
       <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-      <Button className="rounded-circle" variant="outline-dark">!</Button>
+      <Button ref={target} className="rounded-circle" variant='outline-dark'>!</Button>
       </OverlayTrigger>
       <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-      <Button className="rounded-circle" variant="outline-dark">!</Button>
+      <Button ref={target} className="rounded-circle" variant='outline-dark'>!</Button>
       </OverlayTrigger>
     </div>
     </>
